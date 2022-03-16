748011be4c7a131546f6f85aecf933494229eb42
Eveline Guerrero <evelineguerrero23@gmail.com>
2022-03-15 20:13:59 -0400

    commit tree 5387ad29f9f46ff15184ad9112eef230c72f558c

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+def initialize (word)
+    @word = word
+end
+#instances should respond to a #match instance method that 
+#takes an array of possible anagrams. It should return all 
+#matches in an array. 
+#If no matches exist, it should return an empty array.
+def matches (array)
+    
+    
+end
+end
